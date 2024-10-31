02294b3121cba144392b99061324c84fd1ce9599
Shahidul <theshahidkhan.2004@gmail.com>
2024-10-31 12:06:57 +0600

    updated the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/login/login'
+import Chat from './pages/chat/chat'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 const App = () => {
   return (
-  <div>
-    App
-  </div>
+  <>
+  <Routes>
+    <Route path="/" element={<Login/>} />
+    <Route path="/chat" element={<Chat/>} />
+    <Route path="/profile" element={<ProfileUpdate/>} />
+  </Routes>
+  </>
   )
 }
 
