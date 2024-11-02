1c959dd9af38137dd962a87bd89fee193c01780a
Shahidul <theshahidkhan.2004@gmail.com>
2024-11-02 12:13:09 +0600

    updated chat-box

--- src/components/chatBox/chatBox.jsx
+++ src/components/chatBox/chatBox.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import "./ChatBox.css";
+import assets from "../../assets/assets";
 
 const ChatBox = () => {
     return (
-    <div>ChatBox</div>
+    <div className="chat-box">
+        <div className="chat-user">
+            <img src={assets.profile_img} alt="" />
+            <p>Richard Sanford <img src={assets.green_dot} className="dot" alt="" /></p>
+            <img src={assets.help_icon} className="help" alt="" />
+        </div>
+        <div className="chat-input">
+            <input type="text" placeholder="Send a text" />
+            <input type="file" id="image" accept="image/png, image/jpeg" hidden/>
+            <label htmlFor="image">
+                <img src={assets.gallery_icon} alt="" />
+            </label>
+            <img src={assets.send_button} alt="" />
+        </div>
+    </div>
     );
 };
 
