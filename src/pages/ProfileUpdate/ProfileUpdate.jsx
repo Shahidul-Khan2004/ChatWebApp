5b0776d96a619a9253ba86e70315eac37428c2b3
Shahidul <theshahidkhan.2004@gmail.com>
2024-11-03 20:33:26 +0600

    updated profile update page.

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-    return <div>ProfileUpdate</div>;
+
+    const [image, setImage] = useState(false);
+
+    return (
+        <div className="profile">
+            <div className="profile-container">
+                <form>
+                    <h3>Profile Details</h3>
+                    <label htmlFor="avatar">
+                        <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept=".png, .jpg, .jpeg" hidden/>
+                        <img src= {image? URL.createObjectURL(image):assets.avatar_icon} alt="" />
+                        upload profile image
+                    </label>
+                    <input type="text" placeholder="Name" required />
+                    <textarea placeholder="Bio" required></textarea>
+                    <button type ="submit">Save</button>
+                </form>
+                <img className="profile-pic"src="red-chat-logo.png" alt="" />
+            </div>
+        </div>
+    );
 };
 
 export default ProfileUpdate;   
