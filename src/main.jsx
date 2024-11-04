59c43d3ef5595932e5971582f1c445bef99b1f20
Shahidul <theshahidkhan.2004@gmail.com>
2024-11-04 22:27:21 +0600

    added firebase storage logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AppContextProvoder from './context/AppContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AppContextProvoder>
+    <AppContextProvider>
       <App />
-    </AppContextProvoder>
+    </AppContextProvider>
   </BrowserRouter>
 )
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,26 +1,82 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./ProfileUpdate.css";
 import assets from "../../assets/assets";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import { toast } from "react-toastify";
 
 const ProfileUpdate = () => {
 
-    const [image, setImage] = useState(false);
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [bio, setBio] = useState("");
+    const [uid, setUid] = useState("");
+    const [prevImage, setPrevImage] = useState("");
+    const {setUserData} = useContext(AppContext);
+
+    const updateProfile = async (event) => {
+        event.preventDefault();
+        try {
+            const docRef = doc(db, "users", uid);
+            if (prevImage) {
+                await updateDoc(docRef, {
+                    avatar: prevImage,
+                    name: name,
+                    bio: bio
+                })
+            }
+            else {
+                await updateDoc(docRef, {
+                    name: name,
+                    bio: bio
+                })
+            }
+            const snap = await getDoc(docRef);
+            setUserData(snap.data());
+            navigate("/chat");
+        } catch (error) {
+            console.error(error);
+            toast.error(error.message);
+        }
+    }
+
+
+    useEffect(() => {
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                setUid(user.uid)
+                const docRef = doc(db, "users", user.uid);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.data().name) {
+                    setName(docSnap.data().name);
+                }
+                if (docSnap.data().bio) {
+                    setBio(docSnap.data().bio);
+                }
+            }
+            else {
+                navigate("/")
+            }
+        })
+    })
 
     return (
         <div className="profile">
             <div className="profile-container">
-                <form>
+                <form onSubmit={updateProfile}>
                     <h3>Profile Details</h3>
                     <label htmlFor="avatar">
-                        <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept=".png, .jpg, .jpeg" hidden/>
-                        <img src= {image? URL.createObjectURL(image):assets.avatar_icon} alt="" />
-                        upload a square profile image <br /> to avoid distortion
+                        <img src={prevImage ? prevImage : assets.avatar_icon} alt="" />
+                        <input onChange={(e) => setPrevImage(e.target.value)} value={prevImage} type="text" placeholder="Paste Direct Image Link" />
                     </label>
-                    <input type="text" placeholder="Name" required />
-                    <textarea placeholder="Bio" required></textarea>
-                    <button type ="submit">Save</button>
+                    <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Name" required />
+                    <textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder="Bio" required></textarea>
+                    <button type="submit">Save</button>
                 </form>
-                <img className="profile-pic"src={image? URL.createObjectURL(image):"red-chat-logo.png"} alt="" />
+                <img className="profile-pic" src={prevImage ? prevImage : "red-chat-logo.png"} alt="" />
             </div>
         </div>
     );
