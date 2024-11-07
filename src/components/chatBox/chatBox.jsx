63fac0bdb512fb54c7e131540e98d8b877d396b8
Shahidul <theshahidkhan.2004@gmail.com>
2024-11-07 22:39:30 +0600

    added send message functionality

--- src/components/chatBox/chatBox.jsx
+++ src/components/chatBox/chatBox.jsx
@@ -1,7 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./ChatBox.css";
 import assets from "../../assets/assets";
 import { AppContext } from "../../context/AppContext";
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { toast } from "react-toastify";
 
 const ChatBox = () => {
 
@@ -9,11 +12,60 @@ const ChatBox = () => {
 
     const [input, setInput] = useState("");
 
+    const sendMessages = async () => {
+        try {
+            if (input && messagesId) {
+                await updateDoc(doc(db, "messages", messagesId), {
+                    messages: arrayUnion({
+                        sId: userData.id,
+                        text: input,
+                        createdAt: new Date()
+                    })
+                })
+
+                const userIDs = [chatUser.rId,userData.id];
+
+                userIDs.forEach(async (id) => {
+                    const userChatRef = doc(db, "Chats", id);
+                    const userChatSnapshot = await getDoc(userChatRef);
+
+                    if (userChatSnapshot.exists()) {
+                        const userChatData = userChatSnapshot.data();
+                        const chatIndex = userChatData.chatData.findIndex((c) => c.messageId === messagesId);
+                        userChatData.chatData[chatIndex].lastMessage = input.slice(0, 30);
+                        userChatData.chatData[chatIndex].updatedAt = Date.now();
+                        if (userChatData.chatData[chatIndex].rId === userData.id) {
+                            userChatData.chatData[chatIndex].messageSeen = false;                    
+                        }
+                        await updateDoc(userChatRef, {
+                            chatData: userChatData.chatData
+                        })
+                    }
+                })
+    
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+        setInput("");
+    }
+
+    useEffect(() => {
+        if (messagesId) {
+            const unSub = onSnapshot(doc(db,"messages",messagesId), (res) => {
+                setMessages(res.data().messages.reverse())
+            })
+            return () => {
+                unSub();
+            }
+        }
+    }, [messagesId])
+
     return chatUser ? (
         <div className="chat-box">
             <div className="chat-user">
-                <img src={assets.profile_img} alt="" />
-                <p>Richard Sanford <img src={assets.green_dot} className="dot" alt="" /></p>
+                <img src={chatUser.userData.avatar} alt="" />
+                <p>{chatUser.userData.name} <img src={assets.green_dot} className="dot" alt="" /></p>
                 <img src={assets.help_icon} className="help" alt="" />
             </div>
 
@@ -45,12 +97,12 @@ const ChatBox = () => {
 
 
             <div className="chat-input">
-                <input type="text" placeholder="Send a text" />
+                <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder="Send a text" />
                 <input type="file" id="image" accept="image/png, image/jpeg" hidden />
                 <label htmlFor="image">
                     <img src={assets.gallery_icon} alt="" />
                 </label>
-                <img src={assets.send_button} alt="" />
+                <img onClick={sendMessages} src={assets.send_button} alt="" />
             </div>
         </div>
     )
